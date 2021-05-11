a7df33cd496e048b52418c5a2b824ec355b6d38e
keita86 <ke4i2ta4@gmail.com>
2021-05-11 04:09:59 +0000

    [blogのindex作成途中]

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,32 @@
+class BlogsController < ApplicationController
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
+    @blog.save
+    redirect_to blogs_path
+  end
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :image, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about'
+  resources :blogs
   
 end
