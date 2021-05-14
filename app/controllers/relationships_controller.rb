8bf7f341f6135bb01df3b5cfda15f16f2553250b
keita86 <ke4i2ta4@gmail.com>
2021-05-14 04:11:05 +0000

    [部分テンプレート一部作成]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,22 +1,23 @@
 class RelationshipsController < ApplicationController
+  
   def create
     current_user.follow(params[:user_id])
     redirect_to request.referer
-  end 
-  
+  end
+
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
-  end 
+    redirect_to request.referer  
+  end
   
   def followings
-    user = User(params[:user_id])
+    user = User.find(params[:user_id])
     @users = user.followings
-  end 
-  
+  end
+
   def followers
-    user = User(params[:user_id])
-    @users = user.followes
+    user = User.find(params[:user_id])
+    @users = user.followers
   end 
   
 end
