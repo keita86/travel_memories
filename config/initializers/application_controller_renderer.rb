49d66273cc27c5a58feb2d4637d5306bcb769715
keita86 <ke4i2ta4@gmail.com>
2021-05-16 00:09:08 +0000

    [add]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e56d5a6c5750cb6efd6ccc9a9a8f8ad9194f5d340b091a4023a670e2b7a4a90eb6fee68ed0cb0aa8358a044aa47559690e721038088106e7e0006e4e7f16c728'
